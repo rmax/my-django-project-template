f4963b8c05826b76fd38e7c16f412e574f778bd1
Rolando Espinoza La fuente <darkrho@gmail.com>
2009-12-01 19:13:12 -0400

    improved base settings. added site app. south disabled by default.

--- myproject/conf/settings.py
+++ myproject/conf/settings.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Extensible django settings.
+DO NOT ADD LOCAL SETTINGS HERE.
+USE -> conf/local/settings.py
+"""
 # Import global settings to make it easier to extend settings. 
 from django.conf.global_settings import *
 # Import the project module to calculate directories relative to the module
@@ -43,25 +49,63 @@ LOGOUT_URL = '/accounts/logout/'
 LOGIN_REDIRECT_URL = '/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = '' # bin/manage.py generate_secret_key
 
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, PROJECT_MODULE_NAME, 'templates'),
 )
 
-TEMPLATE_CONTEXT_PROCESSORS += (
+# django context processors
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    #'django.core.context_processors.debug', # not enabled by default
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
     'django.core.context_processors.request',
 )
 
+# local context processors
+TEMPLATE_CONTEXT_PROCESSORS += (
+    #'myproject.apps.site.context_processors.common',
+)
+
+# django middleware's
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    #'django.middleware.doc.XViewMiddleware',
+)
+
+# local middleware's
+MIDDLEWARE_CLASSES += (
+)
+
+# django apps
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
+    #'django.contrib.admin',
+    #'django.contrib.admindocs',
     'django.contrib.humanize',
-    'django_extensions',
-    'south',
+    'django.contrib.markup',
+    'django.contrib.sitemaps',
+    'django.contrib.syndication',
+    'django.contrib.webdesign',
 )
+
+# 3rd party apps 
+INSTALLED_APPS += (
+    'django_extensions',
+    #'south', # not full support for sqlite
+)
+
+# project apps
+INSTALLED_APPS += (
+    'myproject.apps.site',
+)
+
